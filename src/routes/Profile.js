2a5100c07b34f58b602e49a592a5d155c0c0a343
pooreum <vnfma0218@naver.com>
2021-06-22 10:39:58 +0900

    sign out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { authService } from '../fbase';
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log out</button>
+      Profile
+    </>
+  );
+};
+export default Profile;
